efec8389970bf61f74053560b9b5cdc67dfe6cc4
Máximo Costa <97714036+Maxidev01@users.noreply.github.com>
2026-04-30 23:39:42 -0300

    3.1

--- backEnd/script.js
+++ backEnd/script.js
@@ -1,1 +1,21 @@
 lucide.createIcons();
+
+const cards = document.querySelectorAll(".tarjetas");
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach((entry) => {
+
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+
+  });
+
+}, {
+  threshold: 0.2
+});
+
+cards.forEach((card) => {
+  observer.observe(card);
+});
