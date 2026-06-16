3a445710267469637da28cf182cdf1942640a54c
FrancescoRamirezC <francescoramirezc1@gmail.com>
2026-06-16 11:33:37 -0300

    0f0sa

--- backEnd/config.php
+++ backEnd/config.php
@@ -6,6 +6,4 @@ return [
     'db_pass' => '/Galpon*Taxi*Pernil5',
     'db_charset' => 'utf8mb4',
     'turnstile_secret_key' => '0x4AAAAAADLmXc6cTwzAnXfzlnTSN5r39-E',
-    'notification_email' => 'contact@scodedigital.com',
-    'from_email' => 'contact@scodedigital.com',
 ];
